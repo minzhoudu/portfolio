b17497e3bb10309792d3ccc48ac592cf16f4dc1e
Pavle <paja98100@gmail.com>
2021-01-23 16:33:26 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// const date = document.querySelector("#date");
+
+// // set year
+// date.innerHTML = new Date().getFullYear();
+
+const navBtn = document.querySelector("#nav-btn");
+const sidebar = document.querySelector("#sidebar");
+const closeBtn = document.querySelector("#close-btn");
+
+const navbar = document.querySelector("#nav");
+
+//! show/hide sidebar
+navBtn.addEventListener("click", function () {
+  sidebar.classList.add("show-sidebar");
+})
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+})
+
+//! fixed navbar
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+})
