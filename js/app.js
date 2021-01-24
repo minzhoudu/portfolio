04740d8a3c74459f30f8430c98783cfc9b4ca564
Pavle <paja98100@gmail.com>
2021-01-24 02:41:29 +0100

    setting navbar

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ closeBtn.addEventListener("click", function () {
 //! fixed navbar
 
 window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 80) {
+  if (window.pageYOffset > 40) {
     navbar.classList.add("navbar-fixed");
   } else {
     navbar.classList.remove("navbar-fixed");
