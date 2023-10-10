af2b7e1afdd2dc9d902c8a6752fde563bc925e54
Pavle <paja98100@gmail.com>
2023-10-10 16:30:18 +0200

    SocialIcons-Transition

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -6,13 +6,27 @@ interface SocialIconsProps {}
 const SocialIcons: FC<SocialIconsProps> = () => {
   return (
     <div className="flex justify-center pb-7">
-      <a href="https://github.com/minzhoudu" target="_blank">
+      <a
+        href="https://github.com/minzhoudu"
+        target="_blank"
+        className="transition-transform duration-300 ease-in-out hover:-translate-y-2"
+      >
         <FaGithub className="mx-6 text-4xl text-white hover:text-primaryLime" />
       </a>
-      <a href="https://www.instagram.com/p.jjovanovic/" target="_blank">
+
+      <a
+        href="https://www.instagram.com/p.jjovanovic/"
+        target="_blank"
+        className="transition-transform duration-300 ease-in-out hover:-translate-y-2"
+      >
         <FaInstagram className="mx-6 text-4xl text-white hover:text-primaryLime" />
       </a>
-      <a href="https://www.linkedin.com/in/jovanovixp/" target="_blank">
+
+      <a
+        href="https://www.linkedin.com/in/jovanovixp/"
+        target="_blank"
+        className="transition-transform duration-300 ease-in-out hover:-translate-y-2"
+      >
         <FaLinkedin className="mx-6 text-4xl text-white hover:text-primaryLime" />
       </a>
     </div>
