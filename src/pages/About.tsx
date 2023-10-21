282516f1f78db312dc55d6e03286b9471e60bee7
Pavle <paja98100@gmail.com>
2023-10-21 12:25:21 +0200

    Fixed about me text and logo size for mobile

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,15 +8,15 @@ interface AboutProps {}
 const About: FC<AboutProps> = () => {
   return (
     <main className="flex w-full flex-col bg-primaryGray pb-10 pt-20">
-      <div className="self-center py-32 text-7xl text-white">
+      <div className="self-center py-32 text-5xl text-white lg:text-7xl">
         <h1>
           <span>About</span>{" "}
           <span className="rounded-lg bg-primaryLime-dark px-4">Me</span>
         </h1>
       </div>
 
-      <div className="mx-auto w-10/12 text-3xl text-white lg:w-3/5">
-        <h1 className="pb-3 text-5xl">
+      <div className="mx-auto w-10/12 text-xl text-white lg:w-3/5 lg:text-3xl">
+        <h1 className="pb-3 text-3xl lg:text-5xl">
           Hello, my name is <span className="text-primaryLime">Pavle</span>
         </h1>
 
