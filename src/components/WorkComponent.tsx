b5e2b4db23ece45b362693700a719f3e8f9dccd6
Pavle <paja98100@gmail.com>
2023-10-30 13:12:15 +0100

    my work page statement added

--- src/components/WorkComponent.tsx
+++ src/components/WorkComponent.tsx
@@ -8,6 +8,10 @@ interface WorkComponentProps {}
 const WorkComponent: FC<WorkComponentProps> = () => {
   return (
     <>
+      <h3 className="w-3/5 pb-10 text-center text-xl text-white underline underline-offset-4 lg:text-2xl">
+        These are some of the projects I worked on
+      </h3>
+
       <div className="hidden flex-col gap-y-20 lg:flex">
         {work.map((item, index) =>
           index % 2 === 0 ? (
